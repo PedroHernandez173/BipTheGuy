0b4d5c21385f627bcea3bebc63fb2ec5fef36c72
PedroHernandez173 <Phernande0152@gmail.com>
2025-04-29 14:22:28 -0700

    Bip The Guy App

--- BipTheGuy/ContentView.swift
+++ BipTheGuy/ContentView.swift
@@ -6,16 +6,46 @@
 //
 
 import SwiftUI
+import AVFAudio
 
 struct ContentView: View {
+    @State private var audioPlayer: AVAudioPlayer!
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Image("clown")
+                .resizable()
+                .scaledToFit()
+                .onTapGesture {
+                    playsSound(soundName: "punchSound")
+                }
+            
+            Spacer()
+            
+            Button {
+                //TODO: button action here
+            } label: {
+                Label("Photo Library", systemImage: "photo.fill.on.rectangle.fill")
+            }
+
         }
         .padding()
+    }
+    
+    func playsSound(soundName: String){
+        if audioPlayer != nil && audioPlayer.isPlaying {
+        audioPlayer.stop()
+    }
+        guard let soundFile = NSDataAsset(name: soundName) else {
+            print("😡 Could not read file named \(soundName)")
+            return
+        }
+        do {
+            audioPlayer = try AVAudioPlayer(data: soundFile.data)
+            audioPlayer.play()
+        } catch {
+            print("😡 ERROR: \(error.localizedDescription) creatingaudioPlayer")
+        }
     }
 }
 
